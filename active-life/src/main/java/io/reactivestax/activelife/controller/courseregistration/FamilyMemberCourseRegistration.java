3f2dbe1bafe8dc470093489caf488be7ef65aac8
Manpreet Kaur <Manpreet.Kaur@fullstacklabs.digital>
2025-01-31 00:36:12 -0500

    added all  controller for Family Course Registrations

--- active-life/src/main/java/io/reactivestax/activelife/controller/courseregistration/FamilyMemberCourseRegistration.java
+++ active-life/src/main/java/io/reactivestax/activelife/controller/courseregistration/FamilyMemberCourseRegistration.java
@@ -1,15 +1,33 @@
 package io.reactivestax.activelife.controller.courseregistration;
 import io.reactivestax.activelife.dto.FamilyCourseRegistrationDTO;
+import io.reactivestax.activelife.dto.FamilyMemberDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/familycourseregistration")
+@RequestMapping("/api/v1/courseregistration")
 public class FamilyMemberCourseRegistration {
 
-    public ResponseEntity<String> addFamilyCourseRegistration(FamilyCourseRegistrationDTO familyCourseRegistrationDTO){
+    @PostMapping("/member")
+    public ResponseEntity<String>  addNewMemberToOfferedCourse(@Valid @RequestBody FamilyCourseRegistrationDTO familyCourseRegistrationDTO) {
+        return ResponseEntity.ok("family member added sucessfully to a course : " );
+    }
+    @GetMapping("/member/{id}")
+    public ResponseEntity<FamilyCourseRegistrationDTO> getMemberDetailsWithId(@RequestHeader("Member-ID") @PathVariable String id) {
+        return ResponseEntity.ok( );
 
+    }
+
+    @PatchMapping("/members")
+    public ResponseEntity<FamilyCourseRegistrationDTO> updateMemberInformation(@RequestBody FamilyCourseRegistrationDTO  familyCourseRegistrationDTO) {
+        return ResponseEntity.ok(familyCourseRegistrationDTO);
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<String> withdrawMemberFromCourse(@PathVariable Long id) {
+
+        return ResponseEntity.ok("family member withdraw from course successfully");
     }
 
 
--- active-life/src/main/java/io/reactivestax/activelife/service/FamilyCourseRegistrationService.java
+++ active-life/src/main/java/io/reactivestax/activelife/service/FamilyCourseRegistrationService.java
@@ -1,5 +1,6 @@
 package io.reactivestax.activelife.service;
 
+import io.reactivestax.activelife.dto.FamilyCourseRegistrationDTO;
 import io.reactivestax.activelife.repository.FamilyCourseRegistrationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -7,5 +8,10 @@ public class FamilyCourseRegistrationService {
 
     @Autowired
     private FamilyCourseRegistrationRepository familyCourseRegistrationRepository;
+
+    public void addfamilyMemberToCourse(FamilyCourseRegistrationDTO familyCourseRegistrationDTO){
+
+
+    }
 
 }
